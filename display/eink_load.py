5f666a36eadb34da3656f5db9a22a12593a43d0e
cire <chan.eric.ch@gmail.com>
2024-01-24 23:19:38 +0800

    Updated to support eink

--- display/eink_load.py
+++ display/eink_load.py
@@ -2,8 +2,10 @@
 # -*- coding:utf-8 -*-
 
 import logging
+import os
+
 from display.waveshare_epd import epd7in3f
-from PIL import Image
+from PIL import ImageFont, ImageDraw, Image
 
 logging.basicConfig(level=logging.INFO)
 
@@ -11,13 +13,20 @@ epd = epd7in3f.EPD()
 epd.init()
 
 
-def load_image(imagePath):
+def load_leaderboard(kv: dict, bg_file, font_file):
     logging.debug("Clearing Image")
     epd.Clear()
-    logging.info("Loading Image @ %s", imagePath)
-    image = Image.open(imagePath)
+    font = ImageFont.truetype(font_file, 24)
+    logging.info("Loading Image @ %s", bg_file)
+    image = Image.open(bg_file)
+    draw = ImageDraw.Draw(image)
+
+    initial_y = 90
+    for a, b in sorted(kv.items(), key=lambda x: x[1], reverse=True):
+        logging.info("{}: {}".format(a, b))
+        draw.text((190, initial_y), "{}: {}".format(a, b), font = font)
+        initial_y = initial_y + 30
     logging.debug("Loading Image Buffer")
     buff = epd.getbuffer(image)
     logging.debug("Loading Image to Display")
     epd.display(buff)
-
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import asyncio
 
+from PIL import Image
 from dotenv import load_dotenv
 import logging
+
 from w3 import connection, monitor_contract
 import os
 import urllib.request
@@ -12,20 +14,31 @@ ssl_context.load_verify_locations(certifi.where())
 
 load_dotenv()
 
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'img')
+fontdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'font')
+
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
 
-async def print(mc):
-    while True:
-        for a, b in sorted(mc.getAddressBalance().items(), key=lambda x:x[1], reverse=True):
-            logging.info("{}: {}".format(a, b))
-        await asyncio.sleep(10)
-
 
 def get_monitor_addresses():
     return urllib.request.urlopen(os.environ["MONITOR_ACCOUNT_PATH"], context=ssl_context).read().decode('utf-8').splitlines()
+
+
+async def load_image(mc):
+    try:
+        imagePath = os.path.join(picdir, 'framebg.jpg')
+        logging.info("Loading Image @ %s", imagePath)
+        fontPath = os.path.join(fontdir, 'SourceSans3-Regular.ttf')
+        logging.info("Loading Font @ %s", fontPath)
+        from display import eink_load
+        while True:
+            eink_load.load_leaderboard(mc.getAddressBalance(), imagePath, fontPath)
+            await asyncio.sleep(10)
+    except RuntimeError as e:
+        logging.warning("Error launching eink, skipping for now: {}".format(e))
 
 
 async def start():
@@ -41,5 +54,6 @@ async def start():
     await t2
 
 if __name__ == '__main__':
+    load_image()
     asyncio.run(start())
 
